1076b6ac0995a23563df5dc499732bddae217fb4
Doug Wolf <douglaswolf@yahoo.com>
2022-03-28 19:01:02 -0700

    See History.h for changes

--- src/main.v
+++ src/main.v
@@ -3,8 +3,11 @@
 `define COUNTER_WIDTH 16
 
 
+
+
+
 //======================================================================================================================
-// pos_edge_detector() - Detects highgoing edges of a pin (for instance, a button)
+// button() - Detects highgoing edges of a pin (for instance, a button)
 //
 // Input:  clk = system clock
 //         pin = the pin to look for a high-going edge on
@@ -14,22 +17,29 @@
 // Notes: edge detection is fully debounced.  q only goes high if a specified pin is still high
 //        10ms after the high-going edge was initially detected 
 //======================================================================================================================
-module pos_edge_detector(input clk, input pin, output q);
+module button(input clk, input pin, output q);
     
     parameter [31:0] DEBOUNCE_PERIOD = `SYSCLOCK_FREQ / 200;
+    
+    logic [2:0] button_sync = 0;
 
-    reg previous_pin_state = 0;
     reg [31:0] debounce_clock = 0;
     reg edge_detected = 0;    
     
     // We're going to check for edges on every clock cycle
     always @(posedge clk) begin
-                
+
+        // Bit 2 is the oldest reliable state
+        // Bit 1 is the newst reliable state
+        // Bit 0 should be considered metastable        
+        button_sync = (button_sync << 1) | pin;
+        
+        // Presume for the moment that we haven't detected the active-going edge of the button        
         edge_detected <= 0;       
         
         // If the debounce clock is about to expire, find out of the user-specicied pin is still high
         if (debounce_clock == 1) begin
-            edge_detected <= pin;
+            edge_detected <= button_sync[1];
             debounce_clock <= 0;
         end
         
@@ -39,11 +49,7 @@ module pos_edge_detector(input clk, input pin, output q);
         end  
         
         // If the pin is high and was previously low, start the debounce clock
-        if (pin & ~previous_pin_state) debounce_clock <= DEBOUNCE_PERIOD;
-        
-        // The 'previous_pin_state" register gets the current state of the pin for 
-        // use during the next clock cycle`
-        previous_pin_state <= pin;
+        if (button_sync[2:1] == 2'b01) debounce_clock <= DEBOUNCE_PERIOD;
     end
     
     // The output wire always reflects the state of the 'edge_detected' register
@@ -54,111 +60,6 @@ endmodule
 
 
 
-/*
-//======================================================================================================================
-// main() - Top level module
-//======================================================================================================================
-module main
-    (
-        input clk,
-        input rst_n,
-        input BTNU,
-        
-        output LED16_B,
-        
-        // The 16 green LEDs
-        output [15:0] led,
-        
-        // The anodes of the rightmost four 7-segment displays 
-        output [7:0] AN,
-        
-        // THe cathods of the 7-segment dispalsy
-        output [7:0] SEG
-    );
-  
-    // States that our FSM walks thru
-    parameter s_IDLE         = 0;
-    parameter s_WAIT_FOR_BCD = 1;
-   
-    // This has a '1' on every clock cycle for which a high-going button-edge is detected
-    wire w_button_edge;
-
-    // Variable that tracks how many times the button has been pressed
-    reg [`COUNTER_WIDTH-1:0] r_counter = 0;
-    
-    // The current state of our FSM
-    reg r_state = s_IDLE;
-
-    // On any clock cycle that this is a '1', the BCD FSM starts
-    reg r_start_bcd_engine = 0;
-    
-    // This is the output of the binary_to_bcd module, and contains our count expressed in BCD
-    wire [15:0] w_bcd;
-    
-    // When the conversion to BCD is complete, the output of the BCD module gets stored here
-    reg  [15:0] r_bcd;
-    
-    // A flag that contains a '1' whenever a BCD conversion is complete and 'w_bcd' holds valid data
-    wire w_dv;
-    
-    // This detects fully debounced high-going edges from the button
-    pos_edge_detector u1(clk, BTNU, w_button_edge);
-
-    // A FSM that converts the binary value in 'r_counter' into BCD stored in 'w_bcd'
-    binary_to_bcd#(.INPUT_WIDTH(16), .DECIMAL_DIGITS(4)) u2(clk, r_counter, r_start_bcd_engine, w_bcd, w_dv);
-    
-    // A block of 4 7-segment displays
-    seven_seg u3(clk, r_bcd, SEG, AN); 
-
-    always @(posedge clk) begin
-      
-        // By default, we aren't starting the BCD engine this clock cycle
-        r_start_bcd_engine <= 0;
-      
-        // The FSM that stores our count in BCD into the r_bcd register
-        case (r_state)
-
-        // We're waiting for w_button_edge to go high           
-        s_IDLE:
-          begin 
-            if (w_button_edge)  begin
-              r_counter          <= r_counter + 1;
-              r_start_bcd_engine <= 1;
-              r_state            <= s_WAIT_FOR_BCD;
-            end
-          end
-
-        //  We're waiting for the BCD conversion to complete         
-        s_WAIT_FOR_BCD:
-          begin
-            if (w_dv) begin
-              r_bcd   <= w_bcd;
-              r_state <= s_IDLE;
-            end
-          end
-                  
-        endcase
-        
-        // The reset button resets the counter to 0
-        if (!rst_n) begin
-          r_counter <= 0;
-          r_state   <= s_IDLE;
-        end
-        
-    end 
-                 
-  
-    //assign led = r_counter;
-    assign led = r_bcd;
-    
-    // Light up the blue LED whenever our pushbutton is being pressed
-    assign LED16_B = BTNU;
-  
-    
-endmodule
-//======================================================================================================================
-*/
-
 
 
 //======================================================================================================================
@@ -190,7 +91,7 @@ module main
     reg [`COUNTER_WIDTH-1:0] r_counter = 0;
     
     // This detects fully debounced high-going edges from the button
-    pos_edge_detector u1(clk, BTNU, w_button_edge);
+    button u1(clk, BTNU, w_button_edge);
 
     // This displays a value on a four-digit 7-segment display
     four_digit_display u2(clk, r_counter, SEG, AN);
